00e03cf8f48c95010c7c264529e569c831cfb4e7
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-01-03 00:52:32 +0900

    test

---
+++ Assets/script/stage_reload.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class stage_reload : MonoBehaviour
+{
+    bool reload;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(reload);
+        if (reload == true)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    public void back()
+    {
+        reload = true;
+    }
+}
