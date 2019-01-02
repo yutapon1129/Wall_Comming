4901572fbd6db17b95ade79b805a2598b7e627e4
yutapon1129 <momo_anbasa@yahoo.co.jp>
2019-01-03 00:20:42 +0900

    Gitらないでね♡

--- Assets/script/camera.cs
+++ Assets/script/camera.cs
@@ -2,16 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class camera : MonoBehaviour
-{
+public class camera : MonoBehaviour {
+
     public GameObject player;
 
-    private void Awake()
-    {
-        
-    }
-
     // Use this for initialization
+<<<<<<< HEAD
 <<<<<<< HEAD
     void Start () {
         player = GameObject.Find("player");
@@ -22,10 +18,14 @@ public class camera : MonoBehaviour
         player = GameObject.Find("player");    
 >>>>>>> 4f3955d8af792feb9e18b19e3ae1ed3a0a65bc91
     }
+=======
+    void Start () {
+        player = GameObject.Find("player");
+	}
+>>>>>>> parent of 08fee75... uiload完成
 	
 	// Update is called once per frame
-	void FixedUpdate ()
-    {
+	void FixedUpdate () {
 
         transform.position = new Vector3(player.transform.position.x, 0, -100);
 
