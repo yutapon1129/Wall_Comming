064874acfe394d6ce7ac335a33a5f9329fa11dc0
yutapon1129 <momo_anbasa@yahoo.co.jp>
2019-01-03 00:51:33 +0900

    ♡

--- Assets/script/camera_title.cs
+++ Assets/script/camera_title.cs
@@ -9,7 +9,7 @@ public class camera_title : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        player = GameObject.Find("player");
     }
 
     // Update is called once per frame
