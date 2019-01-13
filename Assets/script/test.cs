816c51bf8a075877f5fb7a9d1a2bdd0c9b25a894
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-01-13 23:45:10 +0900

    背景slide

--- Assets/script/test.cs
+++ Assets/script/test.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public bool followcamera;
+    public float offset;
+    private Vector3 oldpos;
+    private Vector3 oldcampos;
+
+    // Use this for initialization
+    void Start()
+    {
+        oldpos = transform.position;
+        oldcampos = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        transform.Translate(-0.005f, 0, 0);
-        if (transform.position.x < -240)
+        if (followcamera)
         {
-            transform.position = new Vector3(240, 0, 0);
+            Vector3 v = new Vector3((Camera.main.transform.position.x - oldcampos.x) / offset, 0, 0);
+            transform.localPosition = oldpos + v;
+        }
+        else
+        {
+            Vector3 v = new Vector3((oldcampos.x - Camera.main.transform.position.x) / offset, 0, 0);
+            transform.localPosition = oldpos + v;
         }
     }
 }
