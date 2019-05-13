9313cdef450a7e9973610abb867d607053cade3d
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-05-14 02:56:33 +0900

    05/14
    w1-1~w1-3までのボスの
    グラフィック実装
    
    ボススクリプトのspeedをfloat化
    
    各ステージの微修正

--- Assets/script/boss.cs
+++ Assets/script/boss.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class boss : MonoBehaviour {
 
     Rigidbody2D rigidbody2D;
-    public int speed = -3, x = 1;
+    public float speed = -3, x = 1;
     public GameObject explosion;
 
     public int flag;
