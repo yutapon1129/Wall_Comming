77dd10c8f68062b1a8bfe57fbf9f22a331e6761c
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-05-28 03:53:56 +0900

    05/28-2
    ムービーのスクリプト制御実装

---
+++ Assets/script/movie/start_movie.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class start_movie : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public int starttime;
+
+    void Start()
+    {
+        //同じゲームオブジェクトにあるPlayableDirectorを取得する
+        playableDirector = GetComponent<PlayableDirector>();
+        PlayTimeline();
+    }
+
+    //再生する
+    void PlayTimeline()
+    {
+        playableDirector.Play();
+        playableDirector.time = starttime;
+    }
+
+    //一時停止する
+    void PauseTimeline()
+    {
+        playableDirector.Pause();
+    }
+
+    //一時停止を再開する
+    void ResumeTimeline()
+    {
+        playableDirector.Resume();
+    }
+
+    //停止する
+    void StopTimeline()
+    {
+        playableDirector.Stop();
+    }
+
+}
