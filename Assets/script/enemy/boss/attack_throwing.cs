b53b75417c07d7833ed97d8074e0b9db4196c40a
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-07-19 03:23:23 +0900

    07/19-1
    w1-5完成

---
+++ Assets/script/enemy/boss/attack_throwing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack_throwing : MonoBehaviour
+{
+    public GameObject item; //投げるｵﾌﾞｼﾞｪｸﾄ
+
+    public float timeOut;           //攻撃頻度の時間
+    private float timeElapsed;      //時間計測変数格納用
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.timeScale;//時間計測
+        Debug.Log(timeElapsed);
+        if (timeElapsed >= timeOut)//設定した時間になったら読み込み
+        {
+            Instantiate(item, transform.position, transform.rotation);
+            timeElapsed = 0.0f;//変数リセット用
+        }
+    }
+}
---
+++ Assets/script/enemy/boss/boss_bone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss_bone : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public int speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        //skeletonの向きに飛ばす
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        Destroy(gameObject, 60);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
