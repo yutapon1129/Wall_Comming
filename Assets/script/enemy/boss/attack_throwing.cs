73b44095e1ecae28685785718b08e126dc714213
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-07-26 02:08:51 +0900

    07/26-1
    selectのクリスタル有効化+壁
    
    ※全ステージ通してのテストプレイして

--- Assets/script/enemy/boss/attack_throwing.cs
+++ Assets/script/enemy/boss/attack_throwing.cs
@@ -9,12 +9,6 @@ public class attack_throwing : MonoBehaviour
     public float timeOut;           //攻撃頻度の時間
     private float timeElapsed;      //時間計測変数格納用
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
