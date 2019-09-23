d828baf3d02be65a6ea2de473fdf2247b0a43819
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-09-24 01:54:22 +0900

    09/24-1
    selectのワープ実装
    カメラの左右の移動上限をUnityから指定可能に

---
+++ Assets/script/select/Select_warp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select_warp : MonoBehaviour
+{
+    [SerializeField] string select_name;
+    [SerializeField] GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("player");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            FadeManager.Instance.LoadScene(select_name, 2.0f);
+            player.SetActive(false);
+        }
+    }
+}
