7ab6f9676669b2dc36627bb60f8bd0dce0645644
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-10-04 05:53:27 +0900

    動く床（インスタント版）
    machineを置いてスピード設定したらその通りに

---
+++ Assets/script/stage_gimmick/prefab_machine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefab_machine : MonoBehaviour
+{
+    [SerializeField] GameObject floor;  //生み出す床
+    [SerializeField] float speed, deletetime; //床の移動速度と消す時間
+
+    public float timeOut;           //生成頻度
+    private float timeElapsed;      //時間計測変数格納用//ｶﾒﾗ真偽
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeElapsed += Time.timeScale;//時間計測
+
+        if (timeElapsed >= timeOut)//設定した時間になったら読み込み
+        {
+            GameObject a = Instantiate(floor, transform.position, transform.rotation) as GameObject;
+
+            move_instant A = a.GetComponent<move_instant>();
+            A.Create(speed, deletetime);
+
+            timeElapsed = 0.0f;//変数リセット用
+        }
+    }
+}
