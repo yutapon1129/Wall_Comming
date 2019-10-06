ead58fdd08f5c72dbd253276983a2f27ee0cdd85
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-10-07 04:03:39 +0900

    w2_3ほぼ完成　＋　prefab_machine仕様変更
    w2_3
    　　敵とアイテム配置で一応完成
    
    prefab_machine
    　　ゲームを開始したらすぐ床を出るように
    　　最初に出る床を遅延させる機能追加

--- Assets/script/stage_gimmick/prefab_machine.cs
+++ Assets/script/stage_gimmick/prefab_machine.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class prefab_machine : MonoBehaviour
 {
-    [SerializeField] GameObject floor;  //生み出す床
-    [SerializeField] float speed, deletetime; //床の移動速度と消す時間
+    [SerializeField] GameObject floor;          //生み出す床
+    [SerializeField] float speed, deletetime;   //床の移動速度と消す時間
+    [SerializeField] float delay;               //最初の床を出すまでの時間
 
     public float timeOut;           //生成頻度
     private float timeElapsed;      //時間計測変数格納用//ｶﾒﾗ真偽
+    private bool floor_bool;        //遅延処理用
+
+    void Start()
+    {
+        Invoke("first_floor", delay);
+        timeElapsed = timeOut;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        timeElapsed += Time.timeScale;//時間計測
-
-        if (timeElapsed >= timeOut)//設定した時間になったら読み込み
+        if (floor_bool)
         {
-            GameObject a = Instantiate(floor, transform.position, transform.rotation) as GameObject;
+            timeElapsed += Time.timeScale;//時間計測
 
-            move_instant A = a.GetComponent<move_instant>();
-            A.Create(speed, deletetime);
+            if (timeElapsed >= timeOut)//設定した時間になったら読み込み
+            {
+                GameObject a = Instantiate(floor, transform.position, transform.rotation) as GameObject;
 
-            timeElapsed = 0.0f;//変数リセット用
+                move_instant A = a.GetComponent<move_instant>();
+                A.Create(speed, deletetime);
+
+                timeElapsed = 0.0f;//変数リセット用
+            }
         }
+    }
+
+    void first_floor()
+    {
+        floor_bool = true;
     }
 }
