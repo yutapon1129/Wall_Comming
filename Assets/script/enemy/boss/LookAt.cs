fec903dd38bda051fa29da3f461ce7bfd158bff1
yutapon1129 <momo_anbasa@yahoo.co.jp>
2019-10-30 22:01:44 +0900

    W2-6のボス攻撃
    とりあえずコード共有するためにCommitします

---
+++ Assets/script/enemy/boss/LookAt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField] GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAt2D(obj);
+    }
+
+    void LookAt2D(GameObject target)
+    {
+        // 指定オブジェクトと回転さすオブジェクトの位置の差分(ベクトル)
+        Vector3 pos = target.transform.position - transform.position;
+        // ベクトルのX,Yを使い回転角を求める
+        float angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+        Quaternion rotation = new Quaternion();
+        // 回転角は右方向が0度なので-90しています
+        rotation.eulerAngles = new Vector3(0, 0, angle - 90);
+        // 回転
+        transform.rotation = rotation;
+    }
+}
