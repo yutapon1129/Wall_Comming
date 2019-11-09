a310e233286b6065ae997bea17c27dc705188d6b
yutapon1129 <momo_anbasa@yahoo.co.jp>
2019-11-10 02:17:24 +0900

    W2-6
    完成
    
    ギミック多め、W3-1，3-2より難しくしてる。

--- Assets/script/enemy/boss/B_FB_W.cs
+++ Assets/script/enemy/boss/B_FB_W.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class B_FB : MonoBehaviour
+public class B_FB_W : MonoBehaviour
 {
     private GameObject player;
     [SerializeField] float speed = 2.5f;
@@ -14,8 +14,14 @@ public class B_FB : MonoBehaviour
         ////player取得
         player = GameObject.Find("player");
 
-        // バースト用弾の速度処理-----バースト弾のところに↓入れてね
-        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+        // Rigidbody取得
+        rid2d = GetComponent<Rigidbody2D>();
+        // 角度を考慮して弾の速度計算
+        Vector2 bulletV = rid2d.velocity;
+        bulletV.x = Velocity_0 * Mathf.Cos(theta);
+        bulletV.y = Velocity_0 * Mathf.Sin(theta);
+        rid2d.velocity = bulletV;
+
         Destroy(gameObject, 30);
     }
 
