5329bd40a10887e30f04c2aad12a8e668f12dc59
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-11-10 02:35:40 +0900

    w3_stagebase その２作成

---
+++ Assets/script/stage_gimmick/EX/W3_BackgroundMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W3_BackgroundMove : MonoBehaviour
+{
+    [SerializeField] GameObject Camera;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y, -1);
+    }
+}
