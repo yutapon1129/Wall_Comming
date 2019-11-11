88c48e2326fb8d13ed3b8abe9d88586e9c6befb6
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-11-12 05:13:35 +0900

    w3_EXstagebaseデモ完成　Cinemachine追加
    新しいカメラのデモつくった。
    そのカメラのアセット的なのを導入した。

--- Assets/script/Camera_w3EX.cs
+++ Assets/script/Camera_w3EX.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Camera_w3EX : MonoBehaviour
 {
-    public GameObject player;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject mine;
+    [SerializeField] float camera_Up, camera_Down;
+    [SerializeField] float camera_L, camera_R;
 
     // Use this for initialization
     void Start()
     {
         player = GameObject.Find("player");
+        mine = this.gameObject;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1.5f, -1);
+
+        if (transform.position.x > camera_R)
+        {
+            transform.position = new Vector3(camera_R, mine.transform.position.y, -1);
+        }
+
+        if (transform.position.x < camera_L)
+        {
+            transform.position = new Vector3(camera_L, mine.transform.position.y, -1);
+        }
+
+        if (transform.position.y < camera_Down)
+        {
+            transform.position = new Vector3(mine.transform.position.x, camera_Down, -1);
+        }
+
+        if(transform.position.y > camera_Up)
+        {
+            transform.position = new Vector3(mine.transform.position.x, camera_Up, -1);
+        }
     }
 }
---
+++ Assets/script/ui_system/player/aaaaaaaaaaaaaaaaaaaaaaaaa.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class aaaaaaaaaaaaaaaaaaaaaaaaa : MonoBehaviour
+{
+
+    public CinemachineVirtualCamera vcamera;
+
+    public GameObject PPPPP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PPPPP = GameObject.Find("player");
+        //Vector3 itemp = PPPPP.transform.position;
+        //Transform TTTtemp = itemp;
+
+        vcamera.Follow = PPPPP.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
