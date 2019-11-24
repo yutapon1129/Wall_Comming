a2902a9fd83d9aaa23ca50423cc052f3b1c2f00c
oturu3 <46247826+oturu3@users.noreply.github.com>
2019-11-24 20:45:19 +0900

    w3_6微修正

--- Assets/script/ui_system/player/Player_movingfloor_system.cs
+++ Assets/script/ui_system/player/Player_movingfloor_system.cs
@@ -6,22 +6,29 @@ public class Player_movingfloor_system : MonoBehaviour
 {
     [SerializeField] GameObject Player_Parent;
 
-    //動く床に乗ったら乗った床の子オブジェクトに
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Move")
-        {
-            Debug.Log("お”お”お”お”お”お”ん”");
-            Player_Parent.GetComponent<PlayerMove>().Floor_Enter(other.gameObject.transform);
-        }
-    }
+    ////動く床に乗ったら乗った床の子オブジェクトに
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.gameObject.tag == "Move")
+    //    {
+    //        Debug.Log("お”お”お”お”お”お”ん”");
+    //        Player_Parent.GetComponent<PlayerMove>().Floor_Enter(other.gameObject.transform);
+    //    }
+    //}
     void OnTriggerExit2D(Collider2D other)
     {
-
         if (transform.parent != null && other.gameObject.tag == "Move")
         {
             Player_Parent.GetComponent<PlayerMove>().Floor_Exit();
+        }
+    }
 
+    //動く床に乗ったら乗った床の子オブジェクトに
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Move")
+        {
+            Player_Parent.GetComponent<PlayerMove>().Floor_Enter(other.gameObject.transform);
         }
     }
 }
